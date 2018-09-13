027cfdb1beebc7446f9715bcf73e1843d23dcf95
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-09-12 19:16:38 -0500

    Initial Commit

---
+++ FrameOfMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrameOfMind
+//
+//  Created by RAWLS, JENNIFER AMANDA [AG/1000] on 9/12/18.
+//  Copyright © 2018 RAWLS, JENNIFER AMANDA [AG/1000]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
